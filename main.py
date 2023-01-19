820fe03285827c73da694dbbc37262f20f26384d
Fifek252 <fifkul@student.agh.edu.pl>
2023-01-19 19:59:23 +0100

    gotowe sprawozdanie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import Union, Callable
+from typing import Union, Callable, List
 
 
 def solve_euler(fun: Callable, t_span: np.array, y0: np.array):
@@ -16,10 +16,20 @@ def solve_euler(fun: Callable, t_span: np.array, y0: np.array):
     (np.array): macierz o wymiarze (n,m) zawierająca w w kolumnach kolejne rozwiązania fun w czasie t_span.  
 
     '''
-    wynik = []
-    wynik.append(y0)
-    for n in range(0, len(t_span) - 1):
-        h = (t_span[n + 1] - t_span[n])
-
-        wynik.append(wynik[n] + np.array(fun(t_span[n], wynik[n])) * h)
-    return wynik
+    if not isinstance(fun, Callable) or not isinstance(t_span, (List, np.ndarray)) or not isinstance(y0, (List, np.ndarray)):
+        return None
+    
+    h = t_span[1] - t_span[0]
+    
+    if len(y0.shape) == 2:
+        result = np.zeros(shape=(t_span.shape[0]+1, y0.shape[1]))
+        result[0] = y0
+        for i, t in enumerate(t_span):
+            result[i+1] = result[i] + h*fun(t, result[i])
+        return result[:-1]
+    else:
+        result = np.zeros(shape=(t_span.shape[0]+1))
+        result[0] = y0
+        for i, t in enumerate(t_span):
+            result[i+1] = result[i] + h*fun(t, result[i])
+        return result[:-1]
